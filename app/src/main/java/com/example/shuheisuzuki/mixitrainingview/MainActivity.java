a8e6eac07abcd90cb9f70334e3152d77c0ef2ba1
Shuhei Suzuki <hum2.dmg@gmail.com>
2015-01-15 02:09:43 +0900

    add butterknife libs

--- app/src/main/java/com/example/shuheisuzuki/mixitrainingview/MainActivity.java
+++ app/src/main/java/com/example/shuheisuzuki/mixitrainingview/MainActivity.java
@@ -8,61 +8,58 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.crashlytics.android.Crashlytics;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
+import io.fabric.sdk.android.Fabric;
+
 
 public class MainActivity
         extends ActionBarActivity
-    implements View.OnClickListener
 {
-    private Button linearLayoutPractice1Btn, linearLayoutPractice2Btn,
-            RelativeLayoutPractice1Btn, RelativeLayoutPractice2Btn,
-            FrameLayoutPractice1Btn, FrameLayoutPractice2Btn,
-            ScrollLayoutPractice1Btn, CalclatorPracticeBtn,
-            FragmentPracticeBtn;
+    @InjectView(R.id.linearLayoutPractice1Btn)
+    Button mLinearLayoutPractice1Btn;
+    @InjectView(R.id.linearLayoutPractice2Btn)
+    Button mLinearLayoutPractice2Btn;
+    @InjectView(R.id.RelativeLayoutPractice1Btn)
+    Button mRelativeLayoutPractice1Btn;
+    @InjectView(R.id.RelativeLayoutPractice2Btn)
+    Button mRelativeLayoutPractice2Btn;
+    @InjectView(R.id.FrameLayoutPractice1Btn)
+    Button mFrameLayoutPractice1Btn;
+    @InjectView(R.id.FrameLayoutPractice2Btn)
+    Button mFrameLayoutPractice2Btn;
+    @InjectView(R.id.ScrollLayoutPractice1Btn)
+    Button mScrollLayoutPractice1Btn;
+    @InjectView(R.id.CalclatorPracticeBtn)
+    Button mCalclatorPracticeBtn;
+    @InjectView(R.id.FragmentPracticeBtn)
+    Button mFragmentPracticeBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
-
-        linearLayoutPractice1Btn = (Button) findViewById(R.id.linearLayoutPractice1Btn);
-        linearLayoutPractice2Btn = (Button) findViewById(R.id.linearLayoutPractice2Btn);
-
-        RelativeLayoutPractice1Btn = (Button) findViewById(R.id.RelativeLayoutPractice1Btn);
-        RelativeLayoutPractice2Btn = (Button) findViewById(R.id.RelativeLayoutPractice2Btn);
-
-        FrameLayoutPractice1Btn = (Button) findViewById(R.id.FrameLayoutPractice1Btn);
-        FrameLayoutPractice2Btn = (Button) findViewById(R.id.FrameLayoutPractice2Btn);
-
-        ScrollLayoutPractice1Btn = (Button) findViewById(R.id.ScrollLayoutPractice1Btn);
-
-        CalclatorPracticeBtn = (Button) findViewById(R.id.CalclatorPracticeBtn);
-
-        FragmentPracticeBtn = (Button) findViewById(R.id.FragmentPracticeBtn);
-
-        linearLayoutPractice1Btn.setOnClickListener(this);
-        linearLayoutPractice2Btn.setOnClickListener(this);
-
-        RelativeLayoutPractice1Btn.setOnClickListener(this);
-        RelativeLayoutPractice2Btn.setOnClickListener(this);
-
-        FrameLayoutPractice1Btn.setOnClickListener(this);
-        FrameLayoutPractice2Btn.setOnClickListener(this);
-
-        ScrollLayoutPractice1Btn.setOnClickListener(this);
-
-        CalclatorPracticeBtn.setOnClickListener(this);
-
-        FragmentPracticeBtn.setOnClickListener(this);
+        ButterKnife.inject(this);
     }
 
-    @Override
-    public void onClick(View v)
+    @OnClick({
+            R.id.linearLayoutPractice1Btn,   R.id.linearLayoutPractice2Btn,
+            R.id.RelativeLayoutPractice1Btn, R.id.RelativeLayoutPractice2Btn,
+            R.id.FrameLayoutPractice1Btn,    R.id.FrameLayoutPractice2Btn,
+            R.id.ScrollLayoutPractice1Btn,   R.id.CalclatorPracticeBtn,
+            R.id.FragmentPracticeBtn
+    })
+    public void onClickButton(View v)
     {
         Intent intent = new Intent();
 
         int id = v.getId();
-        switch(id) {
+        switch (id) {
             case R.id.linearLayoutPractice1Btn:
                 intent.setClass(MainActivity.this, LinearLayoutPractice1Activity.class);
                 break;
